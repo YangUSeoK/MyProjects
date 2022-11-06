1133260c32e7932c25b627e0f58f3959a96960ae
DESKTOP-8HCO6QV\Yang <whaqlwnl0@naver.com>
2022-11-07 03:45:32 +0900

    수직 방향조절 추가

--- VolumeLight/Assets/VolumeLight.cs
+++ VolumeLight/Assets/VolumeLight.cs
@@ -8,77 +8,51 @@ public class VolumeLight : MonoBehaviour
     // Ray 관련
     private Light m_Light = null;
     private float m_LightRange = 0f;
-    
+
     // 레이를 쪼개는 비율. 클수록 잘게 쪼갠다
     [SerializeField] private float m_MeshRate = 0.1f;
 
 
     // Mesh 생성관련. 20221106 양우석 : 나중에 클래스 쪼갤 수도 있음.
     private Mesh mLightMesh = null;
-    private MeshFilter mLightMeshFilter = null;
-
-
-    private void Awake()
-    {
-        m_Light = GetComponent<Light>();
-        m_LightRange = m_Light.range;
-
-        mLightMesh = new Mesh();
-        mLightMesh.name = "LightMesh";
-        mLightMeshFilter = GetComponentInChildren<MeshFilter>();
-        mLightMeshFilter.mesh = mLightMesh;
-    }
-
-
-    private void LateUpdate()
-    {
-        DrawFOV();
-    }
+    private MeshCollider m_MeshCollider = null;
 
 
     // ray의 정보를 저장하는 구조체
-    public struct RaycastInfo
+    private struct RaycastInfo
     {
-        public bool hit;
+        public bool isHit;
         public Vector3 pointPos;
-        public float range;
+        public float distance;
         public float angle;
 
         // 생성자
-        public RaycastInfo(bool _hit, Vector3 _pointPos, float _range, float _angle)
+        public RaycastInfo(bool _isHit, Vector3 _pointPos, float _distance, float _angle)
         {
-            hit = _hit;
+            isHit = _isHit;
             pointPos = _pointPos;
-            range = _range;
+            distance = _distance;
             angle = _angle;
         }
     }
 
-    // tarnsform의 Euler.y 값을 인자로 받아와서, 그 방향으로 Ray를 쏴서 hit 정보를 구조체에 저장한다.
-    private RaycastInfo SetRaycastInfo(float _angleDegree)
+    private void Awake()
     {
-        Vector3 dir = DirFromAngle(_angleDegree, true);
+        m_Light = GetComponent<Light>();
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, dir, out hit, m_LightRange))
-        {
-            return new RaycastInfo(true, hit.point, hit.distance, _angleDegree);
-        }
-        else
-        {
-            return new RaycastInfo(false, m_LightRange * transform.position + dir , m_LightRange, _angleDegree);
-        }
+        // 프로퍼티에 계속 접근하는거 싫어서 변수에 저장.
+        m_LightRange = m_Light.range;
+
+        mLightMesh = new Mesh();
+        mLightMesh.name = "LightMesh";
+        GetComponentInChildren<MeshFilter>().mesh = mLightMesh;
+        m_MeshCollider = GetComponentInChildren<MeshCollider>();
+        m_MeshCollider.sharedMesh = mLightMesh;
     }
 
-    // 각도를 수치로 입력받으면 내 현재 각도에서 그만큼 회전한 방향의 방향벡터를 반환한다.
-    public Vector3 DirFromAngle(float _angleDegree, bool _angleIsGlobal)
+    private void LateUpdate()
     {
-        if (!_angleIsGlobal)
-        {
-            _angleDegree += transform.eulerAngles.y;
-        }
-
-        return new Vector3(Mathf.Cos((-_angleDegree + 90) * Mathf.Deg2Rad), 0, Mathf.Sin((-_angleDegree + 90) * Mathf.Deg2Rad)).normalized;
+        DrawFOV();
     }
 
     private void DrawFOV()
@@ -91,12 +65,13 @@ public class VolumeLight : MonoBehaviour
 
         List<Vector3> lightPointList = new List<Vector3>();
 
-        for(int i = 0; i <= stepCnt; ++i)
+        // 부채꼴로 레이를 쏴서 구자체에 정보를 저장한다.
+        for (int i = 0; i <= stepCnt; ++i)
         {
             float angle = transform.eulerAngles.y - (m_Light.spotAngle / 2) + (stepAngleSize * i);
 
             // 디버그용 DrawLine
-            //Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * m_Light.spotAngle, Color.green);
+            //Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * m_LightRange, Color.green);
 
             RaycastInfo newRayCastInfo = SetRaycastInfo(angle);
             lightPointList.Add(newRayCastInfo.pointPos);
@@ -107,22 +82,26 @@ public class VolumeLight : MonoBehaviour
         int vertexCnt = lightPointList.Count + 1;
         Vector3[] vertices = new Vector3[vertexCnt];
 
-        // 20221106 양우석 : 이부분 배열맵핑으로 수정할 수 있음. 내일 할 것
-        int[] triangles = new int[(vertexCnt - 2) * 3]; // 삼각형 개수 vertexCnt-2  꼭지점 개수 : 삼각형개수 *3
+
+
+        // 삼각형 개수 : vertexCnt-2
+        // 삼각형 꼭지점의 수 : 삼각형 개수 * 3
+        int[] triangles = new int[(vertexCnt - 2) * 3];
 
         // 유니티 메시는 로컬좌표가 기준이다.
         vertices[0] = Vector3.zero;
 
-        for(int i = 0; i < vertexCnt-1; ++i)
+        for (int i = 0; i < vertexCnt - 1; ++i)
         {
-            vertices[i+1] = transform.InverseTransformPoint(lightPointList[i]);
+            // InverseTransformPoint : 로컬좌표 위치 받아오기.
+            vertices[i + 1] = transform.InverseTransformPoint(lightPointList[i]);
 
             // 배열맵핑으로 수정
-            if(i<vertexCnt - 2)
+            if (i < vertexCnt - 2)
             {
                 triangles[i * 3] = 0;
-                triangles[i * 3 + 1] = i+ 1;
-                triangles[i * 3 + 2] = i+ 2;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
             }
         }
 
@@ -131,9 +110,41 @@ public class VolumeLight : MonoBehaviour
         mLightMesh.triangles = triangles;
         mLightMesh.RecalculateNormals();
 
+        // 콜라이더는 aabb tree를 사용하기 때문에, 메쉬가 변경되면 다시 빌드해주어야 한다.
+        m_MeshCollider.enabled = false;
+        m_MeshCollider.enabled = true;
     }
 
 
+    // tarnsform의 Euler.y 값을 인자로 받아와서, 그 방향으로 Ray를 쏴서 hit 정보를 구조체에 저장한다.
+    private RaycastInfo SetRaycastInfo(float _angleDegree)
+    {
+        Vector3 dir = DirFromAngle(_angleDegree, transform.eulerAngles.x, true);
 
-    
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, dir, out hit, m_LightRange))
+        {
+            return new RaycastInfo(true, hit.point, hit.distance, _angleDegree);
+        }
+        else
+        {
+            return new RaycastInfo(false, transform.position + (dir * m_LightRange), m_LightRange, _angleDegree);
+        }
+    }
+
+    // 각도를 수치로 입력받으면 내 현재 각도에서 그만큼 회전한 방향의 방향벡터를 반환한다.
+    // 
+    private Vector3 DirFromAngle(float _angleDegree, float _verticalAngleDegree, bool _angleIsGlobal)
+    {
+        if (!_angleIsGlobal)
+        {
+            _angleDegree += transform.eulerAngles.y;
+            _verticalAngleDegree += transform.eulerAngles.x;
+        }
+        
+        // y값(세로 회전) 구면좌표계로 벡터 구하는걸로 수정해야 함.
+        return new Vector3(Mathf.Cos((-_angleDegree + 90f) * Mathf.Deg2Rad), 2f * Mathf.Sin(-_verticalAngleDegree * Mathf.Deg2Rad), Mathf.Sin((-_angleDegree + 90f) * Mathf.Deg2Rad));
+    }
+
+
 }
